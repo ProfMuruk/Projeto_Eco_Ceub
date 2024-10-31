aef863b12f5607764ab35d44b76e8f131a49fc65
Luan Muruk Ferreira Tonaco <luan.tonaco@dinfor.uniceub>
2024-10-31 08:41:42 -0300

    pagina register

--- app.py
+++ app.py
@@ -14,5 +14,9 @@ def sobre():
 def login():
     return render_template('login.html')
 
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
