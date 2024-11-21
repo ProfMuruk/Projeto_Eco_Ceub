39fdc11ff067a5ebc00efbaf79bb5512249c6a01
Luan Muruk Ferreira Tonaco <luan.tonaco@dinfor.uniceub>
2024-11-21 08:32:12 -0300

    alterações e criação do banco de dados

--- app.py
+++ app.py
@@ -1,6 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+import os
+from datetime import datetime
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'seu_segredo'
+
+#caminho para o banco de dados
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'instance', 'ecoceub.db')
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+class Usuario(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.string(100), nullable=False)
+    email = db.Column(db.string(100), nullable=False)
+    senha = db.Column(db.string(20), nullable=False)
+    
+class Evento(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    titulo = db.Column(db.String(100), nullable=False)
+    descricao = db.Column(db.Text, nullable=False)
+    data_evento = db.Column(db.Date, nullable=False)
+    usuario_id = db.Column(db.Integer, db.ForeignKey('usuario.id'), nullable=False)
 
 @app.route('/')
 def hello():
@@ -14,8 +43,14 @@ def sobre():
 def login():
     return render_template('login.html')
 
-@app.route('/register')
+@app.route('/register', methods=['Get', 'Post'])
 def register():
+    if request.method == 'Post':
+        nome = request.form.get('nome')
+        email = request.form.get('email')
+        senha = request.form.get('senha')
+        novo_usuario = Usuario(nome = nome, email = email, senha = senha)
+        
     return render_template('register.html')
 
 if __name__ == '__main__':
